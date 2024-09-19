8a145a0bf5e380696de2d32df48feacd08451c44
StephenCahillane <stephencahillane1@gmail.com>
2024-09-19 22:09:35 +0100

    sites features

--- src/Websites.js
+++ src/Websites.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import MainToolbar from './Toolbar';
 import { Card, CardContent, Typography, Button } from '@mui/material';
-
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import MouseIcon from '@mui/icons-material/Mouse';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import AppShortcutIcon from '@mui/icons-material/AppShortcut';
+import BookIcon from '@mui/icons-material/Book';
 
 function Websites() {
     return (
@@ -11,7 +16,39 @@ function Websites() {
             <div className='sitesPage'>
 
                 <Typography sx={{ color: 'white', textAlign: 'center', fontSize: '24px', mt: 5 }}>Strength2Heal Counselling</Typography>
-                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' }}>
+                <Typography sx={{ color: '#39FF14', textAlign: 'center', fontSize: '16px' }}>Key Features</Typography>
+
+                <List sx={{ color: 'white', textAlign: 'left', maxWidth: '600px' }}>
+
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <MarkEmailReadIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Appointment Booking with Email Notifications." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <RemoveRedEyeIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Pixel installed to capture visitor data." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <AppShortcutIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Backend CRM support with automated outreach processes." />
+                    </ListItem>
+                </List>
+                <Button
+                    variant="contained"
+                    sx={{ backgroundColor: '#39FF14', color: 'white', '&:hover': { backgroundColor: '#32e912' } }}
+                >
+                    Contact Reference
+                </Button>
+
+
+
+                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)', border: '2px solid #39FF14' }}>
                     <CardContent>
 
                         <iframe
@@ -24,7 +61,36 @@ function Websites() {
                     </CardContent>
                 </Card>
                 <Typography sx={{ color: 'white', textAlign: 'center', fontSize: '24px', mt: 5 }}>Sugradh Creche</Typography>
-                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' }}>
+                <Typography sx={{ color: '#39FF14', textAlign: 'center', fontSize: '16px' }}>Key Features</Typography>
+
+                <List sx={{ color: 'white', textAlign: 'left', maxWidth: '600px' }}>
+
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <MarkEmailReadIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Appointment Booking with Email Notifications." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <RemoveRedEyeIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Pixel installed to capture visitor data." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <AppShortcutIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Backend CRM support with automated outreach processes." />
+                    </ListItem>
+                </List>
+                <Button
+                    variant="contained"
+                    sx={{ backgroundColor: '#39FF14', color: 'white', '&:hover': { backgroundColor: '#32e912' } }}
+                >
+                    Contact Reference
+                </Button>
+                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)', border: '2px solid #39FF14' }}>
                     <CardContent>
                         <iframe
                             title="Embedded Website"
@@ -36,7 +102,42 @@ function Websites() {
                     </CardContent>
                 </Card>
                 <Typography sx={{ color: 'white', textAlign: 'center', fontSize: '24px', mt: 5 }}>One Source Business Development</Typography>
-                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' }}>
+                <Typography sx={{ color: '#39FF14', textAlign: 'center', fontSize: '16px' }}>Key Features</Typography>
+
+                <List sx={{ color: 'white', textAlign: 'left', maxWidth: '600px' }}>
+
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <MarkEmailReadIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Appointment Booking with Email Notifications." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <RemoveRedEyeIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Pixel installed to capture visitor data." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <AppShortcutIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Backend CRM support with automated outreach processes." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <BookIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Blog and Vlog updating." />
+                    </ListItem>
+                </List>
+                <Button
+                    variant="contained"
+                    sx={{ backgroundColor: '#39FF14', color: 'white', '&:hover': { backgroundColor: '#32e912' } }}
+                >
+                    Contact Reference
+                </Button>
+                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)', border: '2px solid #39FF14' }}>
                     <CardContent>
                         <iframe
                             title="Embedded Website"
@@ -48,8 +149,36 @@ function Websites() {
                     </CardContent>
                 </Card>
                 <Typography sx={{ color: 'white', textAlign: 'center', fontSize: '24px', mt: 5 }}>Profit Phantoms Digital Community</Typography>
+                <Typography sx={{ color: '#39FF14', textAlign: 'center', fontSize: '16px' }}>Key Features</Typography>
 
-                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' }}>
+                <List sx={{ color: 'white', textAlign: 'left', maxWidth: '600px' }}>
+
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <MarkEmailReadIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Discovery Call Booking with Email Notifications." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <RemoveRedEyeIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Pixel installed to capture visitor data." />
+                    </ListItem>
+                    <ListItem sx={{ backgroundColor: '#1c2027', mt: 1 }}>
+                        <ListItemIcon>
+                            <AppShortcutIcon sx={{ color: '#39FF14' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Fully Built Out App Suite" />
+                    </ListItem>
+                </List>
+                <Button
+                    variant="contained"
+                    sx={{ backgroundColor: '#39FF14', color: 'white', '&:hover': { backgroundColor: '#32e912' } }}
+                >
+                    Contact Reference
+                </Button>
+                <Card sx={{ width: '90%', mt: 5, mb: 5, backgroundColor: '#282c34', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)', border: '2px solid #39FF14' }}>
                     <CardContent>
                         <iframe
                             title="Embedded Website"
